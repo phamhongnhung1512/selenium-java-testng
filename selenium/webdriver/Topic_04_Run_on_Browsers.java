4b15094208664210a63cc57430f91e2ebf0ba844
phamhongnhung <phamhongnhung1512.phn@gmail.com>
2023-01-29 11:26:50 +0700

    Run_on_Browsers

--- selenium/webdriver/Topic_04_Run_on_Browsers.java
+++ selenium/webdriver/Topic_04_Run_on_Browsers.java
@@ -1,41 +1,28 @@
 package webdriver;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Topic_04_Run_on_Browsers {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 
-	@BeforeClass
-	public void beforeClass() {
+	@Test
+	public void TC_01_Run_on_FirefoxDriver() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
+		driver.quit();
+		}
+
+	@Test
+	public void TC_02_Run_on_ChromeDriver() {
+		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.quit();
+		}
+
 	}
-
-	@Test
-	public void TC_01_ValidateCurrentUrl() {
-		}
-
-	@Test
-	public void TC_02_ValidatePageTitle() {
-		}
-
-	@Test
-	public void TC_03_LoginFormDisplayed() {
-		}
-
-	@AfterClass
-	public void afterClass() {
-		}
-}
