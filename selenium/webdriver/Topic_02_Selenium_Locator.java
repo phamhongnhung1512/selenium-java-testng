a7c9ed5916d8f85444797761fdf6afde3067d2df
phamhongnhung <phamhongnhung1512.phn@gmail.com>
2023-01-29 11:18:02 +0700

    Update

---
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -0,0 +1,92 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_02_Selenium_Locator {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://demo.nopcommerce.com/register");
+	}
+
+	@Test
+	public void TC_01_id() {
+		driver.findElement(By.id("FirstName")).sendKeys("Pham");
+		}
+
+	@Test
+	public void TC_02_Class() {
+		driver.get("https://demo.nopcommerce.com/search");
+		driver.findElement(By.className("search-box-text")).sendKeys("Xiaomi Note 8 pro");
+	}
+
+	@Test
+	public void TC_03_name() {
+		driver.findElement(By.name("advs")).click();
+	}
+	@Test
+	public void TC_04_Tagname() {
+		System.out.println(driver.findElements(By.tagName("input")).size());
+	}
+	@Test
+	public void TC_05_LinkText() {
+		driver.findElement(By.linkText("Addresses")).click();
+	}
+	@Test
+	public void TC_06_PartialLinkText() {
+		driver.findElement(By.partialLinkText("vendor account")).click();
+	}
+	
+	@Test 
+	public void TC_07_Css() {
+		driver.get("https://demo.nopcommerce.com/register");
+		driver.findElement(By.cssSelector("input#FirstName")).sendKeys("NganLe");
+		driver.findElement(By.cssSelector("input[id='LastName']")).sendKeys("xinhdep");
+		driver.findElement(By.cssSelector("select[name='DateOfBirthDay']")).sendKeys("12");
+		driver.findElement(By.cssSelector("select[name='DateOfBirthMonth']")).sendKeys("12");
+		driver.findElement(By.cssSelector("select[name='DateOfBirthYear']")).sendKeys("1991");
+		driver.findElement(By.cssSelector("input[name='Email']")).sendKeys("trongtoaletemhettenanh@gmail.com");
+	}
+	
+	@Test 
+	public void TC_08_Xpath() {
+		driver.get("https://demo.nopcommerce.com/register");
+		driver.findElement(By.xpath("//input[@id='Company']")).sendKeys("Sunbeo");
+		driver.findElement(By.xpath("//label[text()='Email:']/following-sibling::input")).sendKeys("girlkuteyeuboydepgiai");
+		//driver.quit();
+	}
+	
+//	@Test
+//	public void TC_09_Xpath() {
+//		driver.get("http://live.techpanda.org/index.php/customer/account/login");
+//		driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//a[@class ='skip-link skip-account']")).click();
+//		driver.findElement(By.xpath("//div[@id ='header-account']//a[@title ='My Account']")).click();
+//		//driver.quit();
+//	}
+	
+	@Test
+	// practice with some text
+	public void TC_10_Xpath() {
+		driver.get("https://automationfc.github.io/basic-form/");
+		System.out.println(driver.findElement(By.xpath("//h5[contains(text(),\"Michael Jackson\")]")).getText());
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		//driver.quit();
+	}
+}
---
+++ selenium/webdriver/Topic_03_XPath_Css.java
@@ -0,0 +1,109 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_03_XPath_Css {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		}
+
+	@Test
+	public void TC_01_Empty_Data() {
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtFirstname-error']")).getText(),"Vui lòng nhập họ tên");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id ='txtEmail-error' and @class ='error']")).getText(),"Vui lòng nhập email");		
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtCEmail-error' and @class='error']")).getText(),"Vui lòng nhập lại địa chỉ email");		
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id ='txtPassword-error' and @class ='error']")).getText(),"Vui lòng nhập mật khẩu");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id ='txtCPassword-error' and @class ='error']")).getText(),"Vui lòng nhập lại mật khẩu");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtPhone-error' and @class ='error']")).getText(),"Vui lòng nhập số điện thoại.");
+
+	}
+	
+	@Test
+	public void TC_02_Invalid_Email() {
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+		driver.findElement(By.xpath("//input[@class='text form-control'and@id='txtFirstname']")).sendKeys("Pham");
+		driver.findElement(By.xpath("//input[@class='text form-control' and @id='txtEmail']")).sendKeys("nhung.123");
+		driver.findElement(By.xpath("//input [@class='text form-control' and @id ='txtCEmail']")).sendKeys("nhung.123");
+		driver.findElement(By.xpath("//input[@class ='text form-control' and @ id='txtPassword']")).sendKeys("nhung123456");
+		driver.findElement(By.xpath("//input[@class='text form-control' and @id='txtCPassword']")).sendKeys("nhung123456");
+		driver.findElement(By.xpath("//input[@class ='text form-control' and @id='txtPhone']")).sendKeys("01234567894");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtEmail-error' and @class='error']")).getText(),"Vui lòng nhập email hợp lệ");		
+		
+	}
+
+	@Test
+	public void TC_03_Incorrect_Email() {
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+		driver.findElement(By.xpath("//input[@class='text form-control'and@id='txtFirstname']")).sendKeys("Pham");
+		driver.findElement(By.xpath("//input[@class='text form-control' and @id='txtEmail']")).sendKeys("nhung@gmail.com");
+		driver.findElement(By.xpath("//input [@class='text form-control' and @id ='txtCEmail']")).sendKeys("nhung123@gmail.com");
+		driver.findElement(By.xpath("//input[@class ='text form-control' and @ id='txtPassword']")).sendKeys("nhung123456");
+		driver.findElement(By.xpath("//input[@class='text form-control' and @id='txtCPassword']")).sendKeys("nhung123456");
+		driver.findElement(By.xpath("//input[@class ='text form-control' and @id='txtPhone']")).sendKeys("01234567894");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("//label [@id='txtCEmail-error' and @class='error']")).getText(),"Email nhập lại không đúng");
+		}
+	
+	@Test
+	public void TC_04_Invalid_Password() {
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+		driver.findElement(By.xpath("//input[@class='text form-control'and@id='txtFirstname']")).sendKeys("Pham");
+		driver.findElement(By.xpath("//input[@class='text form-control' and @id='txtEmail']")).sendKeys("nhung@gmail.com");
+		driver.findElement(By.xpath("//input [@class='text form-control' and @id ='txtCEmail']")).sendKeys("nhung@gmail.com");
+		driver.findElement(By.xpath("//input[@class ='text form-control' and @ id='txtPassword']")).sendKeys("123");
+		driver.findElement(By.xpath("//input[@class='text form-control' and @id='txtCPassword']")).sendKeys("nhung123");
+		driver.findElement(By.xpath("//input[@class ='text form-control' and @id='txtPhone']")).sendKeys("01234567894");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtPassword-error' and @class ='error']")).getText(),"Mật khẩu phải có ít nhất 6 ký tự");
+		}
+	
+	@Test
+	public void TC_05_Incorrect_Password() {
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html"); 
+		driver.findElement(By.xpath("//input[@class='text form-control'and@id='txtFirstname']")).sendKeys("Pham");
+		driver.findElement(By.xpath("//input[@class='text form-control' and @id='txtEmail']")).sendKeys("nhung@gmail.com");
+		driver.findElement(By.xpath("//input [@class='text form-control' and @id ='txtCEmail']")).sendKeys("nhung@gmail.com");
+		driver.findElement(By.xpath("//input[@class ='text form-control' and @ id='txtPassword']")).sendKeys("nhung123456");
+		driver.findElement(By.xpath("//input[@class='text form-control' and @id='txtCPassword']")).sendKeys("123nhung");
+		driver.findElement(By.xpath("//input[@class ='text form-control' and @id='txtPhone']")).sendKeys("01234567894");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtCPassword-error' and @class ='error']")).getText(),"Mật khẩu bạn nhập không khớp");
+		}
+
+	@Test
+	public void TC_06_Invalid_Phone_Number() {
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+		driver.findElement(By.xpath("//input[@class='text form-control'and@id='txtFirstname']")).sendKeys("Pham");
+		driver.findElement(By.xpath("//input[@class='text form-control' and @id='txtEmail']")).sendKeys("nhung@gmail.com");
+		driver.findElement(By.xpath("//input [@class='text form-control' and @id ='txtCEmail']")).sendKeys("nhung@gmail.com");
+		driver.findElement(By.xpath("//input[@class ='text form-control' and @ id='txtPassword']")).sendKeys("nhung123456");
+		driver.findElement(By.xpath("//input[@class='text form-control' and @id='txtCPassword']")).sendKeys("nhung123456");
+		driver.findElement(By.xpath("//input[@class ='text form-control' and @id='txtPhone']")).sendKeys("78123456789");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtPhone-error' and @class ='error']")).getText(),"Số điện thoại bắt đầu bằng: 09 - 03 - 012 - 016 - 018 - 019 - 088 - 03 - 05 - 07 - 08");
+		}
+//	
+	@AfterClass
+	public void afterClass() {
+		//driver.quit();
+	}
+}
